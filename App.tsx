e873782c55b68cc0b28cdaefad4919c65c421213
Leandro Piovezan Dal Molin <leandro.swk@hotmail.com>
2022-09-12 23:36:27 +0100

    Replaces boilerplate text with "Hello World" button
    Getting a sense on how similar React and React Native are

--- App.tsx
+++ App.tsx
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+interface ButtonProps {
+  title: string;
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  );
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Button title='Hello World' />
       <StatusBar style="auto" />
     </View>
   );
